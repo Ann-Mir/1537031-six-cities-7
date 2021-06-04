b4f5df1d09370a7c2588d6b5557fc3c6bdffaaca
Anna Mironovich <anna.mail787@gmail.com>
2021-06-04 15:44:32 +0300

    linter corrections

--- project/src/components/pages/room-page/room-page.jsx
+++ project/src/components/pages/room-page/room-page.jsx
@@ -209,8 +209,10 @@ function RoomPage() {
                       <b className="place-card__price-value">&euro;80</b>
                       <span className="place-card__price-text">&#47;&nbsp;night</span>
                     </div>
-                    <button className="place-card__bookmark-button place-card__bookmark-button--active button"
-                            type="button">
+                    <button
+                      className="place-card__bookmark-button place-card__bookmark-button--active button"
+                      type="button"
+                    >
                       <svg className="place-card__bookmark-icon" style={{width: '18', height: '19'}}>
                         <use xlinkHref="#icon-bookmark"></use>
                       </svg>
