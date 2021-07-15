ba18ee259e36ddeb86fb5f5930d1f82bc05fee3a
Anna Mironovich <anna.mail787@gmail.com>
2021-07-15 14:53:25 +0300

    refactors app.jsx before testing

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -2,49 +2,47 @@ import React from 'react';
 import PrivateRoute from '../private-route/private-route';
 import MainPage from '../pages/main-page/main-page';
 import {AppRoute, AuthorizationStatus} from '../../const';
-import {Switch, Route, Router as BrowserRouter} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import FavoritesPage from '../pages/favorites-page/favorites-page';
 import LoginPage from '../pages/login-page/login-page';
 import RoomPage from '../pages/room-page/room-page';
 import NotFoundPage from '../pages/not-found-page/not-found-page';
-import browserHistory from '../../browser-history';
 
 function App() {
 
   return (
-    <BrowserRouter history={browserHistory}>
-      <Switch>
-        <Route exact path={AppRoute.ROOT}>
-          <MainPage />
-        </Route>
+    <Switch>
+      <Route exact path={AppRoute.ROOT}>
+        <MainPage />
+      </Route>
+      <PrivateRoute
+        exact
+        allowedStatus={AuthorizationStatus.AUTH}
+        path={AppRoute.FAVORITES}
+        redirect={AppRoute.LOGIN}
+        render={() => <FavoritesPage />}
+      >
+      </PrivateRoute>
+      <Route exact path={AppRoute.LOGIN}>
         <PrivateRoute
           exact
-          allowedStatus={AuthorizationStatus.AUTH}
-          path={AppRoute.FAVORITES}
-          redirect={AppRoute.LOGIN}
-          render={() => <FavoritesPage />}
+          allowedStatus={AuthorizationStatus.NO_AUTH}
+          path={AppRoute.LOGIN}
+          redirect={AppRoute.ROOT}
+          render={() => <LoginPage />}
         >
         </PrivateRoute>
-        <Route exact path={AppRoute.LOGIN}>
-          <PrivateRoute
-            exact
-            allowedStatus={AuthorizationStatus.NO_AUTH}
-            path={AppRoute.LOGIN}
-            redirect={AppRoute.ROOT}
-            render={() => <LoginPage />}
-          >
-          </PrivateRoute>
-        </Route>
-        <Route
-          exact
-          path={AppRoute.ROOM}
-          render={() => <RoomPage />}
-        />
-        <Route>
-          <NotFoundPage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+      </Route>
+      <Route
+        exact
+        path={AppRoute.ROOM}
+        render={() => <RoomPage />}
+      />
+      <Route>
+        <NotFoundPage />
+      </Route>
+    </Switch>
+
   );
 }
 
--- project/src/index.js
+++ project/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router as BrowserRouter} from 'react-router-dom';
+import browserHistory from './browser-history';
 import {createAPI} from './services/api';
 import {Provider} from 'react-redux';
 import App from './components/app/app';
@@ -32,7 +34,9 @@ store.dispatch(checkAuth());
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter history={browserHistory}>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
