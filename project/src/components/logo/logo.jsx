2a76001fd3f8def1afcde619f144855880f9bd6c
Anna Mironovich <anna.mail787@gmail.com>
2021-07-19 10:42:59 +0300

    refactors logo

--- project/src/components/logo/logo.jsx
+++ project/src/components/logo/logo.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import {LOGO_SETTINGS} from '../../settings';
 import {AppRoute} from '../../const';
 
 function Logo({ logoType }) {
 
   return (
-    <Link className={`${LOGO_SETTINGS[logoType].CLASS_MIX}__logo-link`} to={AppRoute.ROOT}>
+    <NavLink
+      className={`${LOGO_SETTINGS[logoType].CLASS_MIX}__logo-link`}
+      activeClassName="header__logo-link--active"
+      exact
+      to={AppRoute.ROOT}
+    >
       <img
         className={`${LOGO_SETTINGS[logoType].CLASS_MIX}__logo`}
         src="../img/logo.svg"
         alt="6 cities logo"
         style={{width: LOGO_SETTINGS[logoType].WIDTH, height: LOGO_SETTINGS[logoType].HEIGHT}}
       />
-    </Link>
+    </NavLink>
   );
 }
 
