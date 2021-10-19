6bf76b825ffa635637f3bcd9e07b3c3f3eea240f
Anna <30830631+Ann-Mir@users.noreply.github.com>
2021-10-19 10:56:59 +0300

    Update api.js

--- project/src/services/api.js
+++ project/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'https://7.react.pages.academy/six-cities';
+const BACKEND_URL = 'https://8.react.pages.academy/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
 const HttpCode = {
